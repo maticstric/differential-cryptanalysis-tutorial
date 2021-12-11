4a54b5e9921714b6d3d168b9bc29c08b55383a79
maticstric <matic@likar.me>
2021-12-10 23:13:54 -0800

    add sidebar div, title, logo, and adjust some css

--- js/generate-section-numbers.js
+++ js/generate-section-numbers.js
@@ -4,34 +4,36 @@ sectionLevelNumbers = [0, 0, 0, 0, 0]
 $(':header').each((index, element) => {
   let level = Number(element.nodeName[1]);
 
-  for (let i = 2; i <= 6; i++) {
-    if (level === i) {
-      sectionLevelNumbers[i - 2] += 1;
+  if (level !== 1) {
+    for (let i = 2; i <= 6; i++) {
+      if (level === i) {
+        sectionLevelNumbers[i - 2] += 1;
 
-      for (let j = i - 1; j <= 4; j++) {
-        sectionLevelNumbers[j] = 0;
+        for (let j = i - 1; j <= 4; j++) {
+          sectionLevelNumbers[j] = 0;
+        }
       }
     }
+
+    let sectionNumber = '';
+
+    sectionLevelNumbers.forEach((number) => {
+      if (number !== 0) {
+        sectionNumber += `${number}.`;
+      }
+    });
+
+    sectionNumber = sectionNumber.slice(0, -1);
+
+    // Add section-title span around the section title
+    $(element).contents().filter(function(){
+      return this.nodeType === 3;
+    }).wrap('<span class="section-title"></span>');
+
+    // Add in the section number
+    $(element).prepend(`<span class="section-num">${sectionNumber}</span>`);
+
+    // Add section id to <section>s
+    $(element).parent().prop('id', `section-${sectionNumber.replace('.', '-')}`);
   }
-
-  let sectionNumber = '';
-
-  sectionLevelNumbers.forEach((number) => {
-    if (number !== 0) {
-      sectionNumber += `${number}.`;
-    }
-  });
-
-  sectionNumber = sectionNumber.slice(0, -1);
-
-  // Add section-title span around the section title
-  $(element).contents().filter(function(){
-    return this.nodeType === 3;
-  }).wrap('<span class="section-title"></span>');
-
-  // Add in the section number
-  $(element).prepend(`<span class="section-num">${sectionNumber}</span>`);
-
-  // Add section id to <section>s
-  $(element).parent().prop('id', `section-${sectionNumber.replace('.', '-')}`);
 });
--- js/generate-toc.js
+++ js/generate-toc.js
@@ -1,3 +1,6 @@
+let sidebar = $('#sidebar');
+sidebar.append('<nav id="toc"><ol></ol></nav>')
+
 let tocOl = $('nav#toc > ol');
 
 // previous parent for each h tag level (h2 - h6). Used so you know where to return to if sections go up a level
@@ -8,37 +11,39 @@ let previousLevel = 2;
 $(':header').each((index, element) => {
   let level = Number(element.nodeName[1]);
 
-  for (let i = 2; i <= 6; i++) {
-    if (level === i) {
-      parent = previousOlParent[i - 2];
+  if (level !== 1) {
+    for (let i = 2; i <= 6; i++) {
+      if (level === i) {
+        parent = previousOlParent[i - 2];
 
-      if (previousLevel === i - 1) {
-        let ol = $('<ol></ol>');
-        previousLi.append(ol);
-        parent = ol;
+        if (previousLevel === i - 1) {
+          let ol = $('<ol></ol>');
+          previousLi.append(ol);
+          parent = ol;
+        }
       }
     }
+
+    let sectionNum = $(element).html().match('<span class="section-num">.*?</span>')[0];
+    let sectionTitle = $(element).html().match('<span class="section-title">.*?</span>')[0];
+
+    let li = $('<li></li>');
+    let div = $('<div></div>');
+    div.prop('class', 'section-header');
+
+    li.append(div);
+    div.append(sectionNum);
+
+    let a = $('<a></a>');
+    a.prop('href', `#${$(element).parent().prop('id')}`);
+    a.append(sectionTitle);
+
+    div.append(a);
+
+    parent.append(li);
+
+    previousLevel = level;
+    previousLi = li;
+    previousOlParent[level - 2] = li.parent();
   }
-
-  let sectionNum = $(element).html().match('<span class="section-num">.*?</span>')[0];
-  let sectionTitle = $(element).html().match('<span class="section-title">.*?</span>')[0];
-
-  let li = $('<li></li>');
-  let div = $('<div></div>');
-  div.prop('class', 'section-header');
-
-  li.append(div);
-  div.append(sectionNum);
-
-  let a = $('<a></a>');
-  a.prop('href', `#${$(element).parent().prop('id')}`);
-  a.append(sectionTitle);
-
-  div.append(a);
-
-  parent.append(li);
-
-  previousLevel = level;
-  previousLi = li;
-  previousOlParent[level - 2] = li.parent();
 });
