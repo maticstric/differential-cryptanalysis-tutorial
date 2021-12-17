42e106cb0952ff1cc39624f58a742f4b5cc07c39
maticstric <matic@likar.me>
2021-12-16 22:41:33 -0800

    refactor a bunch of css

--- js/spn-visualization.js
+++ js/spn-visualization.js
@@ -7,7 +7,7 @@ const getPBOX = () => {
   let PBOX = [];
   let valid = true;
 
-  $('#spn-visualization #pbox input').each((index, element) => {
+  $('#spn-visualization > .controls > .pbox input').each((index, element) => {
     if (element.value == '' || element.value.match(notHexRegex) || PBOX.includes(element.value)) {
       valid = false;
     }
@@ -23,7 +23,7 @@ const getSBOX = () => {
   let SBOX = [];
   let valid = true;
 
-  $('#spn-visualization #sbox input').each((index, element) => {
+  $('#spn-visualization > .controls > .sbox input').each((index, element) => {
     if (element.value == '' || element.value.match(notHexRegex) || SBOX.includes(element.value)) {
       valid = false;
     }
@@ -44,19 +44,19 @@ const restrictInput = (event, len) => {
   }
 }
 
-const highlightSvgLine = (step, stepId) => {
+const highlightSvgLine = (step, stepClass) => {
   step.forEach((bit, index) => {
     index = index.toString(16);
 
     if (bit === 1) {
-      $(`#spn-visualization #${stepId} .b${index}`).addClass('isOne');
+      $(`#spn-visualization > .figure > svg .${stepClass} .b${index}`).addClass('isOne');
     }
   });
 }
 
 const highlightSvgLines = (steps) => {
   // Reset all
-  $('#spn-visualization line').removeClass('isOne');
+  $('#spn-visualization > .figure > svg line').removeClass('isOne');
 
   let stepsInBinary = steps.map(step => nibblesToBits(step));
 
@@ -73,13 +73,21 @@ const highlightSvgLines = (steps) => {
 }
 
 const showEncryption = () => {
+
+  let plaintext = $('#spn-visualization > .controls > .plaintext-key-ciphertext .plaintext-input').val();
+  let key1 = $('#spn-visualization > .controls > .plaintext-key-ciphertext .key1-input').val();
+  let key2 = $('#spn-visualization > .controls > .plaintext-key-ciphertext .key2-input').val();
+  let key3 = $('#spn-visualization > .controls > .plaintext-key-ciphertext .key3-input').val();
+  let key4 = $('#spn-visualization > .controls > .plaintext-key-ciphertext .key4-input').val();
+  let key5 = $('#spn-visualization > .controls > .plaintext-key-ciphertext .key5-input').val();
+
   // Put everything into the correct format, which is an array of nibbles
-  let plaintext = $('#spn-visualization #plaintext-input').val().padStart(4, '0').split('').map(n => parseInt(n, 16));
-  let key1 = $('#spn-visualization #key1-input').val().padStart(4, '0').split('').map(n => parseInt(n, 16));
-  let key2 = $('#spn-visualization #key2-input').val().padStart(4, '0').split('').map(n => parseInt(n, 16));
-  let key3 = $('#spn-visualization #key3-input').val().padStart(4, '0').split('').map(n => parseInt(n, 16));
-  let key4 = $('#spn-visualization #key4-input').val().padStart(4, '0').split('').map(n => parseInt(n, 16));
-  let key5 = $('#spn-visualization #key5-input').val().padStart(4, '0').split('').map(n => parseInt(n, 16));
+  plaintext = plaintext.padStart(4, '0').split('').map(n => parseInt(n, 16));
+  key1 = key1.padStart(4, '0').split('').map(n => parseInt(n, 16));
+  key2 = key2.padStart(4, '0').split('').map(n => parseInt(n, 16));
+  key3 = key3.padStart(4, '0').split('').map(n => parseInt(n, 16));
+  key4 = key4.padStart(4, '0').split('').map(n => parseInt(n, 16));
+  key5 = key5.padStart(4, '0').split('').map(n => parseInt(n, 16));
 
   let sbox = getSBOX();
   if (sbox === null) { return; }
@@ -93,7 +101,7 @@ const showEncryption = () => {
 
   // Show ciphertext in controls
   let ciphertextHex = steps[steps.length - 1].map(n => n.toString(16)).join('');
-  $('#spn-visualization #ciphertext-output').text(`0x${ciphertextHex}`);
+  $('#spn-visualization > .controls > .plaintext-key-ciphertext .ciphertext-output').text(`0x${ciphertextHex}`);
 
   // Show ciphertext below svg
   let nibble1 = parseInt(ciphertextHex[0], 16).toString(2).padStart(4, '0');
@@ -101,26 +109,26 @@ const showEncryption = () => {
   let nibble3 = parseInt(ciphertextHex[2], 16).toString(2).padStart(4, '0');
   let nibble4 = parseInt(ciphertextHex[3], 16).toString(2).padStart(4, '0');
 
-  $('#spn-visualization #ciphertext #nibble1').text(nibble1);
-  $('#spn-visualization #ciphertext #nibble2').text(nibble2);
-  $('#spn-visualization #ciphertext #nibble3').text(nibble3);
-  $('#spn-visualization #ciphertext #nibble4').text(nibble4);
+  $('#spn-visualization > .figure > .ciphertext > .nibble1').text(nibble1);
+  $('#spn-visualization > .figure > .ciphertext > .nibble2').text(nibble2);
+  $('#spn-visualization > .figure > .ciphertext > .nibble3').text(nibble3);
+  $('#spn-visualization > .figure > .ciphertext > .nibble4').text(nibble4);
 
   highlightSvgLines(steps);
 }
 
-$(`#spn-visualization #key1-input,
-   #spn-visualization #key2-input,
-   #spn-visualization #key3-input,
-   #spn-visualization #key4-input,
-   #spn-visualization #key5-input`).on('input', (event) => {
+$(`#spn-visualization > .controls > .plaintext-key-ciphertext .key1-input,
+   #spn-visualization > .controls > .plaintext-key-ciphertext .key2-input,
+   #spn-visualization > .controls > .plaintext-key-ciphertext .key3-input,
+   #spn-visualization > .controls > .plaintext-key-ciphertext .key4-input,
+   #spn-visualization > .controls > .plaintext-key-ciphertext .key5-input`).on('input', (event) => {
   restrictInput(event, 4);
 
   showEncryption();
 });
 
 // Put plaintext from input to top of diagram
-$('#spn-visualization #plaintext-input').on('input', (event) => {
+$('#spn-visualization > .controls > .plaintext-key-ciphertext .plaintext-input').on('input', (event) => {
   restrictInput(event, 4);
   let value = event.target.value.padStart(4, '0');
 
@@ -129,22 +137,22 @@ $('#spn-visualization #plaintext-input').on('input', (event) => {
   let nibble3 = parseInt(value[2], 16).toString(2).padStart(4, '0');
   let nibble4 = parseInt(value[3], 16).toString(2).padStart(4, '0');
 
-  $('#spn-visualization #plaintext #nibble1').text(nibble1);
-  $('#spn-visualization #plaintext #nibble2').text(nibble2);
-  $('#spn-visualization #plaintext #nibble3').text(nibble3);
-  $('#spn-visualization #plaintext #nibble4').text(nibble4);
+  $('#spn-visualization > .figure > .plaintext > .nibble1').text(nibble1);
+  $('#spn-visualization > .figure > .plaintext > .nibble2').text(nibble2);
+  $('#spn-visualization > .figure > .plaintext > .nibble3').text(nibble3);
+  $('#spn-visualization > .figure > .plaintext > .nibble4').text(nibble4);
 
   showEncryption();
 });
 
 // Change svg PBOX based on PBOX input
-$('#spn-visualization #pbox input').on('input', (event) => {
+$('#spn-visualization > .controls > .pbox input').on('input', (event) => {
   restrictInput(event, 1);
 
   let PBOX = getPBOX();
 
   if (PBOX !== null) {
-    $('#spn-visualization #pbox-error').hide();
+    $('.visualization > .controls > .error-messages > .pbox-error').hide();
 
     for (let i = 0; i < 16; i++) {
       let classSelector = '.pbox.b' + i.toString(16);
@@ -156,21 +164,21 @@ $('#spn-visualization #pbox input').on('input', (event) => {
 
     showEncryption();
   } else {
-    $('#spn-visualization #pbox-error').show();
+    $('.visualization > .controls > .error-messages > .pbox-error').show();
   }
 });
 
-$('#spn-visualization #sbox input').on('input', (event) => {
+$('#spn-visualization > .controls > .sbox input').on('input', (event) => {
   restrictInput(event, 1);
 
   let SBOX = getSBOX();
 
   if (SBOX !== null) {
-    $('#spn-visualization #sbox-error').hide();
+    $('.visualization > .controls > .error-messages > .sbox-error').hide();
   
     showEncryption();
   } else {
-    $('#spn-visualization #sbox-error').show();
+    $('.visualization > .controls > .error-messages > .sbox-error').show();
   }
 });
 
